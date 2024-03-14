ca13c3568154398483e2b3d8ee08dfad85d37c7f
Li Lin <guru.lin@gmail.com>
2024-03-15 01:23:37 +0800

    added dummy case to generate coverage report for baselib use latest SNAPSHOT version

---
+++ vino-baselib/src/test/java/net/vino9/vino/base/test/DummyTests.java
@@ -0,0 +1,13 @@
+package net.vino9.vino.base.test;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTests {
+
+    @Test
+    public void testDummy() {
+        System.out.println("----dummy test----");
+        System.out.println("----dummy test----");
+        System.out.println("----dummy test----");
+    }
+}
