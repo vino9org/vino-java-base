314446b71c3b05cbbcada980ccfd1119dd57ccfb
Li Lin <guru.lin@gmail.com>
2024-03-27 02:46:27 +0800

    use mockmvc instead of web environment

--- vino-demo-svc/src/test/java/net/vino9/vino/demo/test/HelloServiceTests.java
+++ vino-demo-svc/src/test/java/net/vino9/vino/demo/test/HelloServiceTests.java
@@ -1,24 +1,24 @@
 package net.vino9.vino.demo.test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class HelloServiceTests {
 
-    @LocalServerPort int port;
+    @Autowired MockMvc mockMvc;
 
     @Test
-    void testHelloApi() {
-        var template = new RestTemplate();
-        var svcUrl = "http://localhost:" + port + "/rest/hello";
-
-        assertTrue(template.getForEntity(svcUrl, String.class).getStatusCode().is2xxSuccessful());
+    void testHelloApi() throws Exception {
+        mockMvc.perform(get("/rest/hello")).andExpect(status().is2xxSuccessful());
     }
 }
